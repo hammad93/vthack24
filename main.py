d72109ef42a15e8f28ad1484398826d2ff6b56e6
Hammad Usmani <ham.u@live.com>
2024-09-15 07:11:52 +0000

    Include image generation

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ app = FastAPI()
 def hello():
     return {"Hello": "World"}
 
+@app.get("/")
+def send_report(data, recipient):
+        query = f'''{data}
+    From the above data about weather please generate a HTML email report. Provide nothing but the HTML code.
+    '''
+    result = llm.chat(query)
+    report = result['choices'][0]['message']['content'].replace('\n', '').replace('```', '')
+    img_url = images.generate(f"A nature portrait describing {q}")
+    imag_tag = f'<img src="{img_url}"><br/>'
+    main_mail.send_email(image_tag + report, recipient)
+
 @app.get("/report")
 def report(data):
     '''
